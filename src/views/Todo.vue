aa8e58cf8e2493f95bf861dfbd43a3ea87e0afaa
Gonciarov <gonciarov@yandex.ru>
2021-04-16 11:14:37 +0100

    vuex added

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,8 +3,8 @@
 
         <v-text-field
             v-model="newTaskTitle"
-            @click:append="addTask()"
-            @keyup.enter="addTask()"
+            @click:append='addTask'
+            @keyup.enter='addTask'
             class="pa-3"
             outlined
             label="Add Task"
@@ -14,15 +14,16 @@
           ></v-text-field>
 
     <v-list class="pt-0"
+      v-if="$store.state.tasks.length"
       two-line
       flat
     >
 
     <div
-    v-for="task in tasks"
+    v-for="task in $store.state.tasks"
           :key="task.id">
         <v-list-item
-          @click="doneTask(task.id)"
+          @click="$store.commit('doneTask', task.id)"
           :class="{ 'blue lighten-5' : task.done }">
           <template v-slot:default>
             <v-list-item-action>
@@ -38,7 +39,7 @@
 
           <v-list-item-action>
           <v-btn 
-            @click.stop="deleteTask(task.id)"
+            @click.stop="$store.commit('deleteTask', task.id)"
             icon>
             <v-icon color="primary lighten-1">mdi-delete</v-icon>
           </v-btn>
@@ -50,6 +51,18 @@
     <v-divider></v-divider>
     </div>
     </v-list>
+    <div
+      v-else
+      class="no-tasks"
+      >
+      <v-icon
+      size="100"
+      color="primary"
+      >
+      mdi-check
+      </v-icon>
+      <div class="text-h5 primary--text">No tasks</div>
+    </div>
   </div>
 
 
@@ -63,44 +76,25 @@ export default {
   name: 'Home',
   data() {
     return {
-    newTaskTitle: '',
-    tasks: [
-      {
-        id: 1, 
-        title: "Wake up",
-        done: false
-      },
-      {
-        id: 2, 
-        title: "Coffee",
-        done: false
-      },
-      {
-        id: 3, 
-        title: "Breakfast",
-        done: false
-      }
-    ]
+    newTaskTitle: ''
   }
   },
   methods: {
-    doneTask(id) {
-      let task = this.tasks.filter(task => task.id === id)[0]
-      task.done = !task.done
-    },
     addTask() {
-      let newTask = {
-        id: Date.now(),
-        title: this.newTaskTitle,
-        done: false
-      }
-      this.tasks.push(newTask)
-      this.newTaskTitle = ""
+      this.$store.commit('addTask', this.newTaskTitle)
+      this.newTaskTitle = ''
     },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter(task => task.id != id)
-    }
   }
   }
 
 </script>
+
+<style lang="sass">
+  .no-tasks 
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+  
+
+</style>
